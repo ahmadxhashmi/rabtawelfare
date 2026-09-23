59d858eba76c1f50d404e7dafad0b1a4aca817c8
Muhammad Ahmed Rehman <109869213+ahmadxhashmi@users.noreply.github.com>
2026-09-23 20:18:54 +0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Rabta-Web/',
+  base: '/rabtawelfare/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
